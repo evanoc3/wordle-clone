70836798005453e1f0833e0344edc3468fa6dfef
Evan O'Connor <evanoc3@gmail.com>
2022-01-14 21:51:25 +0000

    Updated word list to use more common words

--- src/data/parser.py
+++ src/data/parser.py
@@ -7,8 +7,8 @@ from random import shuffle
 def main():
 	approved_words: list[str] = []
 
-	# Raw words file sourced from https://raw.githubusercontent.com/dwyl/english-words/master/words.txt
-	raw_words_file_path = Path(__file__).resolve().parent / "words_alpha.txt"
+	# Raw words file sourced from https://github.com/first20hours/google-10000-english/blob/master/google-10000-english-no-swears.txt
+	raw_words_file_path = Path(__file__).resolve().parent / "raw_words.txt"
 	with open(raw_words_file_path, "r") as raw_words_file:
 		line = raw_words_file.readline().strip()
 		while line:
